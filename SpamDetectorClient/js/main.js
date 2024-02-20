994ca52f7ca8c18e80bdfd3908a9748cce3c1eb4
josiah-jehoiakim-yap <josiahjehoiakim.yap@ontariotechu.net>
2024-02-19 21:57:00 -0500

    -CSS formatted based on assignment example

--- SpamDetectorClient/js/main.js
+++ SpamDetectorClient/js/main.js
@@ -1,7 +1,6 @@
 // TODO: onload function should retrieve the data needed to populate the UI
-let divbody;
+let divbody = document.getElementById("body-sec").innerHTML;
 function loadAbout() {
-   divbody = document.getElementById("body-sec").innerHTML;
   document.getElementById("body-sec").innerHTML='<object type="text/html" data="about.html" ' +
     'width="100%" height="100%"></object>';
 
