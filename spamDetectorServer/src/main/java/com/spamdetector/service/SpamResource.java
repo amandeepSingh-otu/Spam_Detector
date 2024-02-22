ef00d1f5ee1ca7a5b2de297a02fbfca82ac36107
kim@fedora <yapkim.work@gmail.com>
2024-02-22 15:04:37 -0500

    -Split getAccuracyAndPrescision() into calculateAccuracy() and calculatePrecision() functions that return Map objects.

--- spamDetectorServer/src/main/java/com/spamdetector/service/SpamResource.java
+++ spamDetectorServer/src/main/java/com/spamdetector/service/SpamResource.java
@@ -109,7 +109,8 @@ public class SpamResource {
         return jsonArray;
     };
 
-    
+   //Previous Working Function split into calculateAccuracy() and calculatePrecision()
+    /*
     private double[] getAccuracyAndPrescision(List<TestFile> results) throws IOException {
         double accuracy=0;
         int truePositive=0;
@@ -126,9 +127,57 @@ public class SpamResource {
                 falsePositive+=1;
             }
         };
-        //TODO: return map object and break into two functions
         return new double[]{(double) (trueNegative + truePositive) / results.size(), (truePositive/(double) (falsePositive+truePositive))};
     };
+    */
+
+
+    //Calculates accuracy and returns a Map object with key string "accuracy" and value of calculated accuracy
+    private Map<String, double> calculateAccuracy(List<TestFile> results) throws IOException {
+        double accuracy=0;
+        int truePositive=0;
+        int trueNegative=0;
+        int falsePositive=0;
+        for( TestFile file: results){
+            if(file.getSpamProbability()<0.5 && file.getActualClass().equals("Ham")){
+                truePositive+=1;
+            }
+            if(file.getSpamProbability()>0.5 && file.getActualClass().equals("Spam")){
+                trueNegative+=1;
+            }
+            if(file.getSpamProbability()<0.5 && file.getActualClass().equals("Spam")){
+                falsePositive+=1;
+            }
+        };
+
+        Map<String, double> map = new map<>();
+        map.put("accuracy" , (double) (trueNegative + truePositive) / results.size() );
+        return map;
+    }
+
+    //Calculates precision and returns a Map object with key string "precision" and value of calculated precision
+    private Map<String Accuracy, Double Value> calculatePrecision(List<TestFile> results) throws IOException {
+        double accuracy=0;
+        int truePositive=0;
+        int trueNegative=0;
+        int falsePositive=0;
+        for( TestFile file: results){
+            if(file.getSpamProbability()<0.5 && file.getActualClass().equals("Ham")){
+                truePositive+=1;
+            }
+            if(file.getSpamProbability()>0.5 && file.getActualClass().equals("Spam")){
+                trueNegative+=1;
+            }
+            if(file.getSpamProbability()<0.5 && file.getActualClass().equals("Spam")){
+                falsePositive+=1;
+            }
+        };
+
+        Map<String, double> map = new map<>();
+        map.put("precision" , (truePositive/((double) (falsePositive+truePositive))) );
+        return map;
+
+    }
 
     private List<TestFile> trainAndTest() throws IOException {
         if (this.detector==null){
